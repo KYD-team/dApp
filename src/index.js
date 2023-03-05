b96546cffc7f9cf315a3aa9f68b7d034f287e1a3
Otto G <5030059+ottodevs@users.noreply.github.com>
2023-03-05 00:17:52 -0700

    add apollo config for github

--- src/index.js
+++ src/index.js
@@ -1,17 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App'
+import { ApolloClient, InMemoryCache, ApolloProvider, gql, HttpLink } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
+import dotenv from 'dotenv'
+import reportWebVitals from './reportWebVitals'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+dotenv.config()
+
+const httpLink = new HttpLink({ uri: 'https://api.github.com/graphql' })
+
+const authLink = setContext((_, { headers }) => {
+  const token = process.env.REACT_APP_GITHUB_TOKEN
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  }
+})
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+})
+
+client
+  .query({
+    query: gql`
+      query {
+        viewer {
+          login
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result))
+
+const root = createRoot(document.getElementById('root'))
+
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>,
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
